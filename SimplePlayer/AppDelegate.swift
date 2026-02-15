f250033ecf7e53a6483a100cc35ed9eaf37ac2b2
Stephen Booth <me@sbooth.org>
2026-02-15 07:44:08 -0600

    Commit changes for replay gain updates

--- SimplePlayer/AppDelegate.swift
+++ SimplePlayer/AppDelegate.swift
@@ -49,7 +49,7 @@ class AppDelegate: NSObject {
         if openPanel.runModal() == .OK {
             do {
                 let rg = try ReplayGainAnalyzer.analyzeAlbum(openPanel.urls)
-                os_log("Album gain %.2f dB, peak %.8f; Tracks: [%{public}@]", rg.0.gain, rg.0.peak, rg.1.map({ (url, replayGain) in String(format: "\"%@\" gain %.2f dB, peak %.8f", FileManager.default.displayName(atPath: url.lastPathComponent), replayGain.gain, replayGain.peak) }).joined(separator: ", "))
+                os_log("Album gain %.2f dB, peak %.6f; Tracks: [%{public}@]", rg.replayGain.gain, rg.replayGain.peak, rg.trackReplayGain.map({ (url, replayGain) in String(format: "\"%@\" gain %.2f dB, peak %.6f", FileManager.default.displayName(atPath: url.lastPathComponent), replayGain.gain, replayGain.peak) }).joined(separator: ", "))
                 let alert = NSAlert()
                 alert.messageText = "Replay Gain Analysis Complete"
                 alert.informativeText = "Check log for details."
